c38473a0fc6d6fbbe78a75a2e4f7a05f5ed31184
Dylan <yourmom12734@gmail.com>
2023-12-18 21:23:28 -0500

    commit

---
+++ FinalProject1/Controllers/VideoController.cs
@@ -0,0 +1,69 @@
+﻿using FinalProject1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public VideoController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        [HttpGet("/videos")]
+        public async Task<IActionResult> GetVideosAsync()
+        {
+            return Ok(await _appDbContext.Videos.ToListAsync());
+        }
+        [HttpGet("/videos/{id}")]
+        public async Task<IActionResult> GetVideosByUserIdAsync(int userId)
+        {
+            var videos = await _appDbContext.Videos.Where(x => x.UserId == userId).toListAsync(x => x.UserId == userId);
+            if (videos == null)
+            {
+                return NotFound();
+            }
+            return Ok(videos);
+        }
+        [HttpPost("/videos")]
+        public async Task<IActionResult> CreateVideo([FromBody] Video video)
+        {
+            video.CreatedDate = DateTime.Now;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _appDbContext.Videos.Add(video);
+            await _appDbContext.SaveChangesAsync();
+            return Ok(video);
+        }
+        [HttpPut("/videos/{id}")]
+        public async Task<IActionResult> UpdateVideoAsync([FromBody]Video video, int id)
+        {
+            var originalVideo= await _appDbContext.Videos.FirstOrDefaultAsync(x => x.Id == id);
+            if(originalVideo == null)
+            {
+                return NotFound();
+            }
+            video.VideosUrl = originalVideo.Url;
+            video.UpdatedDate = DateTime.Now;
+            await _appDbContext.SaveChangesAsync();
+            return Ok(video);
+        }
+        [HttpDelete("/videos/{id}")]
+        public async Task<IActionResult> DeleteVideo(int id)
+        {
+            var video = await _appDbContext.Videos.FirstOrDefaultAsync(x => x.VideoId == id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+            _appDbContext.Videos.Remove(video);
+            await _appDbContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
