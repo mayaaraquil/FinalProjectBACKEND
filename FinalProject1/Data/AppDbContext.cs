832546df68be468cb9844effa187c0ece1ca9aac
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-12-18 20:40:17 -0500

    working playlistcontroller

--- FinalProject1/Data/AppDbContext.cs
+++ FinalProject1/Data/AppDbContext.cs
@@ -2,6 +2,6 @@
 using FinalProject1.Models;
 
 public class AppDbContext : DbContext{    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)    {    }
-    public DbSet<User> Users { get; set; } 
+    public DbSet<User> Users { get; set; }     public DbSet<Playlist> Playlists { get; set; }
     //public DbSet<Product> Products { get; set; }
 }
--- FinalProject1/Models/User.cs
+++ FinalProject1/Models/User.cs
@@ -3,6 +3,7 @@
     public class User: BaseEntity
     {
         public int UserId { get; set; }
+        public string AuthName { get; set; }
         public string Username { get; set; }
         public string ProfilePicture { get; set; }
         public string Bio { get; set; }
